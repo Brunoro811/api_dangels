d7cfa38bb8c8422bda38c5a8bbcd514f989e5e88
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-02-11 17:15:03 -0300

    feat> initial commit

--- app/routes/users/users_route.py
+++ app/routes/users/users_route.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+from app.controllers.users import users_controllers
 
 bp = Blueprint('users', __name__, url_prefix="/users")
 
 bp.get('')()
-bp.post('')()
+bp.post('')(users_controllers.create_users)
 bp.delete('')()
 bp.patch('')()
