decee4b58f72351007e2d180cef5ce377e023e9c
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-02-11 17:24:49 -0300

    feat: adicionado get produtos por id

--- app/routes/products/produtos_routes.py
+++ app/routes/products/produtos_routes.py
@@ -5,6 +5,7 @@ from app.controllers.products import products_controllers
 bp = Blueprint("products", __name__, url_prefix="/products")
 
 bp.get("")(products_controllers.get_product)
+bp.get("<int:id_product>")(products_controllers.get_one_product)
 bp.post("")(products_controllers.create_product)
 bp.patch("<int:id_product>")(products_controllers.update_product)
 bp.delete("<int:id_product>")(products_controllers.delete_product)
