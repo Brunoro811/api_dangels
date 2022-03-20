09907a5a56d9724d0ce649a5e6f3379ef59d30d8
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-20 16:32:56 -0300

    feat: update product finish

--- app/controllers/products/update.py
+++ app/controllers/products/update.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
 from app.decorators import verify_payload
 
-from app.models.product.product_completed import ProductCompletedModel
 from app.models.product.products_model import ProductModel
 
 from app.helpers.get_data_json_multi_part_form import get_files
@@ -24,6 +23,7 @@ from app.helpers.get_data_json_multi_part_form import get_files
         "sale_value_atacado": [float, int],
         "date_start": str,
         "date_end": str,
+        "color": str,
     },
     optional=[
         "cost_value",
@@ -37,56 +37,29 @@ from app.helpers.get_data_json_multi_part_form import get_files
         "sale_value_atacado",
         "date_start",
         "date_end",
+        "color",
     ],
 )
 def update_product(data: dict, id: int):
     session: Session = current_app.db.session
     try:
 
-        keys_product = [
-            "cost_value",
-            "id_category",
-            "id_store",
-            "name",
-            "sale_value_varejo",
-            "quantity_atacado",
-            "sale_value_promotion",
-            "sale_value_atacado",
-            "date_start",
-            "date_end",
-        ]
-        keys_colors = ["variations", "color_name"]
-        keys_sizes_product = ["sizes_product"]
-
         product: ProductModel = ProductModel.query.get(id)
         if not (product):
             raise NoResultFound
 
-        obj_product_completed = ProductCompletedModel.separates_model_for_update(
-            keys_product, keys_colors, keys_sizes_product, data
-        )
-
-        if obj_product_completed["product"]:
-            update_product = dict(obj_product_completed["product"])
-            for key, value in update_product.items():
-                setattr(product, key, value)
-
-        if obj_product_completed["variations"]:
+        if data["variations"]:
             key = None
             value = None
-            for color_size_update in obj_product_completed["variations"]:
+            list_variations = data.pop("variations")
 
-                for product_variations_base_data in product.variations:
-                    for key, value in color_size_update.items():
-                        if (
-                            product_variations_base_data.color
-                            == color_size_update["color"]
-                            and product_variations_base_data.size
-                            == color_size_update["size"]
-                        ):
-                            setattr(product_variations_base_data, key, value)
-                    if product_variations_base_data.color != color_size_update["color"]:
-                        setattr(product_variations_base_data, key, value)
+            for number_item in range(0, len(list_variations)):
+                for key, value in list_variations[number_item].items():
+                    setattr(product.variations[number_item], key, value)
+
+        if data:
+            for key, value in data.items():
+                setattr(product, key, value)
 
         files = get_files()
         if files:
