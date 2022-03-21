33af2fc37f3b88adf51f18235831dcbc91de789d
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-20 21:25:33 -0300

    feat: finish get order for id seller

---
+++ app/controllers/sales/get_order_for_seller.py
@@ -0,0 +1,61 @@
+import datetime
+from operator import and_
+from flask import current_app, jsonify, request
+from http import HTTPStatus
+
+
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+
+from app.models.users.orders_seller import OrdersModel
+
+from app.default import data_month
+
+
+def verify_order_empty_and_delete(list_or_number_id_order: "int or list[int]") -> None:
+    session: Session = current_app.db.session
+    if type(list_or_number_id_order) == list:
+        for id_order in list_or_number_id_order:
+            order: OrdersModel = OrdersModel.query.get(id_order)
+            session.delete(order)
+    else:
+        order: OrdersModel = OrdersModel.query.get(list_or_number_id_order)
+        session.delete(order)
+    session.commit()
+
+
+def get_all_sale_for_id_seller(id: int, month: "int or None" = None):
+    try:
+        month = request.args.get("month", None)
+        date = str(datetime.date.today())
+
+        if month:
+            if int(month) > 12 or int(month) < 1:
+                return {"error": "Month invalid!"}, HTTPStatus.UNPROCESSABLE_ENTITY
+
+            date_start = f"{date[:-5]}{month}-0{data_month[month][0]}1"
+            date_end = f"{date[:-5]}{month}-{data_month[month][1]}"
+        else:
+            date_start = f"{date[:-2]}01"
+            date_end = f"{date[:-2]}31"
+
+        orders: OrdersModel = (
+            OrdersModel.query.filter_by(id_seller=id)
+            .filter(
+                and_(
+                    OrdersModel.date_creation >= date_start,
+                    OrdersModel.date_creation <= date_end,
+                )
+            )
+            .all()
+        )
+
+        return jsonify(orders), HTTPStatus.OK
+    except ValueError as e:
+        return {"error": f"Value {e.args[0]} incorret"}, HTTPStatus.UNPROCESSABLE_ENTITY
+    except NoResultFound:
+        return {"error": "Order not found."}, HTTPStatus.NOT_FOUND
+    except AttributeError:
+        return {"error": "Not found"}, HTTPStatus.NOT_FOUND
+    except Exception as e:
+        raise e
--- app/routes/sales/orders_route.py
+++ app/routes/sales/orders_route.py
@@ -2,13 +2,17 @@ from flask import Blueprint
 
 from app.controllers.sales import orders_controllers
 
-from app.controllers.sales import create_sale, get_one_sale_for_id_order
+from app.controllers.sales import (
+    create_sale,
+    get_one_sale_for_id_order,
+    get_all_sale_for_id_seller,
+)
 
 bp = Blueprint("orders", __name__, url_prefix="/orders")
 
+bp.get("/seller/<int:id>/")(get_all_sale_for_id_seller)
 bp.get("<int:id>")(get_one_sale_for_id_order)
 bp.post("")(create_sale)
 
-bp.get("/seller/<int:id>/")(orders_controllers.get_all_sale_for_id_seller)
 
 bp.delete("<int:id>")(orders_controllers.delete_sale)
