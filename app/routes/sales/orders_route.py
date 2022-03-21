fb70e6d4ffed6ffa09cef4ab17122632c455583d
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-20 22:18:24 -0300

    feat: refactor delete finish

--- app/routes/sales/orders_route.py
+++ app/routes/sales/orders_route.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-from app.controllers.sales import orders_controllers
-
 from app.controllers.sales import (
     create_sale,
     get_one_sale_for_id_order,
@@ -14,6 +12,4 @@ bp = Blueprint("orders", __name__, url_prefix="/orders")
 bp.get("/seller/<int:id>/")(get_all_sale_for_id_seller)
 bp.get("<int:id>")(get_one_sale_for_id_order)
 bp.post("")(create_sale)
-
-
 bp.delete("<int:id>")(delete_sale)
