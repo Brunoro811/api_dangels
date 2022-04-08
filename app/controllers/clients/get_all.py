bfb5111d1aeb8dda29f8ffb1ef94a30a846e60fe
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-04-08 15:00:19 -0300

    feat: finish search name client

--- app/controllers/clients/get_all.py
+++ app/controllers/clients/get_all.py
@@ -8,18 +8,16 @@ from app.models.client.client_model import ClientModel
 
 def get_clients():
 
-    print(request.args.to_dict())
-    print("")
-    print(request.args.get("search"))
-    clients: list[ClientModel] = []
+    clients: list[ClientModel] = None
     search: str = request.args.get("search")
     if search:
         session: Session = current_app.db.session
         clients = (
             session.query(ClientModel)
-            .filter(ClientModel.first_name == search.title())
+            .filter(ClientModel.first_name.like(f"%{search[1:len(search)]}"))
             .all()
         )
+
     else:
         clients = ClientModel.query.all()
 
