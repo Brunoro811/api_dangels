e75ee7b1c0d15c16e9e16dd9eb4521e13e7c47e0
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-04-08 14:24:14 -0300

    feat: search for name finish

--- app/controllers/clients/get_all.py
+++ app/controllers/clients/get_all.py
@@ -1,11 +1,26 @@
-from flask import jsonify
+from flask import current_app, jsonify, request
 from http import HTTPStatus
+from sqlalchemy.orm import Session, Query
 
 
 from app.models.client.client_model import ClientModel
 
 
 def get_clients():
-    clients = ClientModel.query.all()
+
+    print(request.args.to_dict())
+    print("")
+    print(request.args.get("search"))
+    clients: list[ClientModel] = []
+    search: str = request.args.get("search")
+    if search:
+        session: Session = current_app.db.session
+        clients = (
+            session.query(ClientModel)
+            .filter(ClientModel.first_name == search.title())
+            .all()
+        )
+    else:
+        clients = ClientModel.query.all()
 
     return jsonify(clients), HTTPStatus.OK
